01f0ececa72ece4099c05fd992255fe5266061d9
Loug <lucianoguasque@gmail.com>
2025-05-16 23:16:09 -0300

    sv derust

---
+++ src/App.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Header from "./components/Header.svelte";
+</script>
+
+<main>
+  <div class="wrapper">
+    <Header />
+    <h1>Hello, World!</h1>
+  </div>
+</main>
+
+<style>
+  * {
+    box-sizing: border-box;
+  }
+
+  :global(body) {
+    margin: 0;
+    padding: 0;
+  }
+
+  .wrapper {
+    width: 100vw;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,14 @@
+<nav>
+  <button>Home</button>
+  <button>Contact</button>
+  <button>About</button>
+</nav>
+
+<style>
+  nav {
+    outline: 1px solid red;
+    display: inline flex;
+    gap: 1rem;
+    width: 100vw;
+  }
+</style>
