fbd99c7a8168e84f1022f2dee8f7de883d5d216e
Loug522 <lucianoguasque@gmail.com>
2025-06-09 21:49:03 -0300

    restarted this project

--- src/App.svelte
+++ src/App.svelte
@@ -1,37 +1,47 @@
 <script>
-  import Header from "./components/Header.svelte";
-  import Footer from "./components/Footer.svelte";
-  import Main from "./components/Main.svelte";
-  import SideArticles from "./components/SideArticles.svelte";
+  import svelteLogo from './assets/svelte.svg'
+  import viteLogo from '/vite.svg'
+  import Counter from './lib/Counter.svelte'
 </script>
 
-<main class="main-grid">
-  <Header />
-  <div class="main-container">
-    <Main />
-    <div class="sidebar">
-      <SideArticles />
-    </div>
+<main>
+  <div>
+    <a href="https://vite.dev" target="_blank" rel="noreferrer">
+      <img src={viteLogo} class="logo" alt="Vite Logo" />
+    </a>
+    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
+      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
+    </a>
   </div>
-  <Footer />
+  <h1>Vite + Svelte</h1>
+
+  <div class="card">
+    <Counter />
+  </div>
+
+  <p>
+    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
+  </p>
+
+  <p class="read-the-docs">
+    Click on the Vite and Svelte logos to learn more
+  </p>
 </main>
 
 <style>
-  :global(#app, *) {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
   }
-
-  main {
-    width: 100vw;
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
   }
-
-  .main-container {
-    display: flex;
-    gap: 3rem;
-    height: calc(100vh - 6.5rem);
-    margin-inline: 5rem;
-    padding-block: 1rem;
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
   }
 </style>
