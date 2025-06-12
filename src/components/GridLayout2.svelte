004f0592cb5fac7967025759b2fb5396ad3bc024
Loug522 <lucianoguasque@gmail.com>
2025-06-11 22:39:15 -0300

    finished grid 2

--- src/components/GridLayout2.svelte
+++ src/components/GridLayout2.svelte
@@ -9,9 +9,36 @@
 <style>
   .container {
     text-transform: uppercase;
+    width: 500px;
+    height: 500px;
 
     display: grid;
     grid-template-columns: 1fr 2fr 1fr;
-    grid-template-rows: 1fr 5vh 3fr 5vh;
+    grid-template-rows: 1fr 10vh 3fr 10vh;
+  }
+
+  .header {
+    grid-area: 1 / 1 / 2 / 5;
+    background: red;
+  }
+
+  .main {
+    grid-area: 2 / 1 / 3 / 5;
+    background: goldenrod;
+  }
+
+  .menu {
+    grid-area: 3 / 1 / 3 / 4;
+    background: blue;
+  }
+
+  .aside {
+    grid-area: 3 / 3 / 4 / 5;
+    background: green;
+  }
+
+  .footer {
+    grid-area: 4 / 1 / -1 / -1;
+    background: purple;
   }
 </style>
