a101504b07b8df00d30a43c239b8be60af7e8ad2
Loug522 <lucianoguasque@gmail.com>
2025-06-13 05:51:52 -0300

    fixed some conflicts  and merged branches

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,9 @@
 <script>
   import Forms from "./components/Forms.svelte";
-  import Gradients from "./components/Gradients.svelte";
-  import GridLayout from "./components/GridLayout.svelte";
-  import GridLayout2 from "./components/GridLayout2.svelte";
-  import Inspecting from "./components/Inspecting-states.svelte";
 </script>
 
 <main>
   <div class="wrapper">
     <Forms />
   </div>
-
-  <!-- <div class="wrapper">
-    <GridLayout2 />
-    <Gradients />
-  </div> -->
-
-  <!-- <div class="wrapper">
-    <Inspecting />
-  </div>
-  <div class="box-container">
-    <GridLayout />
-  </div>
 </main>
-
-<style>
-  .wrapper {
-    display: inline flex;
-    gap: 1rem;
-  }
-</style>
